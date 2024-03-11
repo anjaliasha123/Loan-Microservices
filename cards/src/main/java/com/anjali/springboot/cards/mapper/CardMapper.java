7874a5ad4f8b7dbcb64fdb9935538e674b949d50
anjaliasha123 <anjaliasha95@gmail.com>
2024-03-11 09:27:19 -0500

    cards microservice

---
+++ cards/src/main/java/com/anjali/springboot/cards/mapper/CardMapper.java
@@ -0,0 +1,26 @@
+package com.anjali.springboot.cards.mapper;
+
+import com.anjali.springboot.cards.dto.CardDto;
+import com.anjali.springboot.cards.entity.Cards;
+
+public class CardMapper {
+    public static CardDto mapToCardsDto(Cards cards, CardDto cardsDto) {
+        cardsDto.setCardNumber(cards.getCardNumber());
+        cardsDto.setCardType(cards.getCardType());
+        cardsDto.setMobileNumber(cards.getMobileNumber());
+        cardsDto.setTotalLimit(cards.getTotalLimit());
+        cardsDto.setAvailableAmount(cards.getAvailableAmount());
+        cardsDto.setAmountUsed(cards.getAmountUsed());
+        return cardsDto;
+    }
+
+    public static Cards mapToCards(CardDto cardsDto, Cards cards) {
+        cards.setCardNumber(cardsDto.getCardNumber());
+        cards.setCardType(cardsDto.getCardType());
+        cards.setMobileNumber(cardsDto.getMobileNumber());
+        cards.setTotalLimit(cardsDto.getTotalLimit());
+        cards.setAvailableAmount(cardsDto.getAvailableAmount());
+        cards.setAmountUsed(cardsDto.getAmountUsed());
+        return cards;
+    }
+}
