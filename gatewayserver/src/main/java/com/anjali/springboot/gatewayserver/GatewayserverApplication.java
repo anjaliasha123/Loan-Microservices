bbf53b177afd46ae097cd758af62cf2f7cc43cc0
anjaliasha123 <anjaliasha95@gmail.com>
2024-05-22 10:11:14 -0500

    implement custome routing gateway server

--- gatewayserver/src/main/java/com/anjali/springboot/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/anjali/springboot/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,36 @@ package com.anjali.springboot.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+	@Bean
+	public RouteLocator roadMapLearnerRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/roadMapLearner/accounts/**")
+						.filters( f -> f.rewritePath("/roadMapLearner/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/roadMapLearner/loans/**")
+						.filters( f -> f.rewritePath("/roadMapLearner/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/roadMapLearner/cards/**")
+						.filters( f -> f.rewritePath("/roadMapLearner/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS")).build();
 	}
 
 }
